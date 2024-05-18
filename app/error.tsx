11b11d5ab5863d062a74a913f0d7d9e1ea72c827
SergeShcherbina <1990sergik27@gmail.com>
2024-05-18 23:32:19 +0300

    first commit

---
+++ app/error.tsx
@@ -0,0 +1,11 @@
+"use client"
+
+const  Error = ({error}: {error: Error}) => {
+  return (
+    <div>
+      <h2 className={"text-red-500"} >Oops... {error.message}</h2>
+    </div>
+  )
+}
+
+export default Error
